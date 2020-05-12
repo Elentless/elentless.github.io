df5fbf3a30ccb439605bb5380af236fe90564b26
MartinListwan <martinc.listwan@Hotmail.com>
2020-05-12 08:37:39 -0700

    Basic template for website

---
+++ _site/js/elentless.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  console.log("ready");
+  $("#mainContent").fadeIn(2000, myFunction);
+});
+
+
+// Example JS function to call back
+function myFunction() {
+  console.log("here");   // The function returns the product of p1 and p2
+}
